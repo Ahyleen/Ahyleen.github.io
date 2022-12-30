fb293a49120674eaaabe97ab226f60a01e2e9026
Ahyleen <62658727+Ahyleen@users.noreply.github.com>
2022-12-30 15:01:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function h(evt) {
+  if (evt.target.tagName === 'BUTTON') {
+    // do not prevent default behavior if the target element is a button
+    return;
+  }
   evt.preventDefault();
   window.removeEventListener('touchstart', h, null);
   window.removeEventListener('click', h, null);
   const songs = ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3', 'song5.mp3', 'song6.mp3', 'song7.mp3']; // list of .mp3 files in the site root folder
-  let currentIndex = Math.floor(Math.random() * songs.length);
-  let song = new Audio();
-  song.src = songs[currentIndex];
-  song.load();
+  let currentIndex = Math.floor(Math.random() * songs.length); // generate a random index
+  let song = new Audio(); // create a new audio element
+  song.src = songs[currentIndex]; // set the src attribute
+  song.load(); // load the audio file
   document.querySelector('.before').remove();
   document.querySelector('.after').style.display = 'block';
 
@@ -33,20 +37,27 @@ function h(evt) {
   }
 
   function playNext() {
-    let nextIndex = Math.floor(Math.random() * songs.length);
+    // play another random .mp3 file that was not played before
+    let nextIndex = Math.floor(Math.random() * songs.length); // generate a new random index
     while (nextIndex === currentIndex) {
+      // keep generating a new index until it is different from the current index
       nextIndex = Math.floor(Math.random() * songs.length);
     }
-    currentIndex = nextIndex;
-    fadeOut(song);
-    song = new Audio();
-    song.src = songs[currentIndex];
-    song.load();
-    fadeIn(song);
+    currentIndex = nextIndex; // update the current index
+    fadeOut(song); // fade out the current audio
+    song = new Audio(); // create a new audio element
+    song.src = songs[currentIndex]; // set the src attribute
+    song.load(); // load the audio file
+    fadeIn(song); // fade in the next audio
   }
 
+  // attach event listeners to the audio element
   song.addEventListener('ended', playNext);
   song.addEventListener('error', playNext);
 
+  // fade in the audio when it starts
   song.volume = 0;
- 
+  fadeIn(song);
+}
+window.addEventListener('touchstart', h);
+window.addEventListener('click', h);
