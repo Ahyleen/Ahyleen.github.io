bc4da0ebaa6df37aee5b6531cb0baf4df0b896d1
Ahyleen <62658727+Ahyleen@users.noreply.github.com>
2022-12-29 23:15:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function h(evt) {
-    evt.preventDefault()
-    window.removeEventListener('touchstart', h, null)
-    window.removeEventListener('click', h, null)
-    const song = new Audio('song.mp3');
-    document.querySelector('.before').remove();
-    document.querySelector('.after').style.display = 'block';
-    song.play();
+  evt.preventDefault();
+  window.removeEventListener('touchstart', h, null);
+  window.removeEventListener('click', h, null);
+  const song = new Audio('song.mp3');
+  song.loop = true;
+  document.querySelector('.before').remove();
+  document.querySelector('.after').style.display = 'block';
+  song.play();
 }
-window.addEventListener('touchstart', h)
-window.addEventListener('click', h)
+
+const links = document.querySelectorAll('a');
+for (let link of links) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location = this.href;
+  });
+}
+
+window.addEventListener('touchstart', h);
