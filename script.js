46bab866e54293c1c11f3e09ff9c4ef4eba53cac
Ahyleen <62658727+Ahyleen@users.noreply.github.com>
2022-12-30 13:36:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function h(evt) {
-    evt.preventDefault()
-    window.removeEventListener('touchstart', h, null)
-    window.removeEventListener('click', h, null)
-    const song = new Audio('song.mp3');
-    song.loop = true;
-    document.querySelector('.before').remove();
-    document.querySelector('.after').style.display = 'block';
-    song.play();
+  evt.preventDefault();
+  window.removeEventListener('touchstart', h, null);
+  window.removeEventListener('click', h, null);
+  const songs = ['song1.mp3', 'song2.mp3', 'song3.mp3']; // list of .mp3 files in the site root folder
+  const index = Math.floor(Math.random() * songs.length); // generate a random index
+  const song = new Audio(songs[index]); // create a new audio element
+  document.querySelector('.before').remove();
+  document.querySelector('.after').style.display = 'block';
+  song.addEventListener('ended', function() {
+    // play another random .mp3 file when the current audio ends
+    const nextIndex = Math.floor(Math.random() * songs.length); // generate a new random index
+    song.src = songs[nextIndex]; // set the src attribute to the new .mp3 file
+    song.play(); // play the audio
+  });
+  song.play();
 }
-window.addEventListener('touchstart', h)
-window.addEventListener('click', h)
+window.addEventListener('touchstart', h);
+window.addEventListener('click', h);
