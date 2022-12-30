81cfb2413f0c5c90e7a0b7977f76b8894a39084b
Ahyleen <62658727+Ahyleen@users.noreply.github.com>
2022-12-30 17:12:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-const songs = ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3', 'song5.mp3', 'song6.mp3', 'song7.mp3']; // list of .mp3 files in the site root folder
-const audioElements = []; // array to store audio elements
-
-for (const song of songs) {
-  // create a new audio element for each song
-  const audio = new Audio(song);
-  audio.preload = 'auto'; // set the preload attribute to 'auto'
-  audioElements.push(audio);
-}
-
 function h(evt) {
   evt.preventDefault();
   window.removeEventListener('touchstart', h, null);
   window.removeEventListener('click', h, null);
-  const index = Math.floor(Math.random() * audioElements.length); // generate a random index
-  let audio = audioElements[index]; // get the audio element at the random index
+  const songs = ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3', 'song5.mp3', 'song6.mp3', 'song7.mp3']; // list of .mp3 files in the site root folder
+  const index = Math.floor(Math.random() * songs.length); // generate a random index
+  const song = new Audio(songs[index]); // create a new audio element
   document.querySelector('.before').remove();
   document.querySelector('.after').style.display = 'block';
-
-  function fadeOut(audio) {
-    if (audio.volume > 0) {
-      audio.volume -= 0.1;
-      setTimeout(function() {
-        fadeOut(audio);
-      }, 100);
-    } else {
-      audio.pause();
-    }
-  }
-
-  function fadeIn(audio) {
-    if (audio.volume < 1) {
-      audio.volume += 0.1;
-      setTimeout(function() {
-        fadeIn(audio);
-      }, 100);
-    } else {
-      audio.play();
-    }
-  }
-
-  audio.addEventListener('ended', function() {
+  song.addEventListener('ended', function() {
     // play another random .mp3 file when the current audio ends
-    const nextIndex = Math.floor(Math.random() * audioElements.length); // generate a new random index
-    fadeOut(audio); // fade out the current audio
-    audio = audioElements[nextIndex]; // get the audio element at the random index
-    fadeIn(audio); // fade in the next audio
+    const nextIndex = Math.floor(Math.random() * songs.length); // generate a new random index
+    song.src = songs[nextIndex]; // set the src attribute to the new .mp3 file
+    song.play(); // play the audio
   });
-  audio.play();
+  song.play();
 }
 window.addEventListener('touchstart', h);
 window.addEventListener('click', h);
